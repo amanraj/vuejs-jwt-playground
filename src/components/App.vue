1653d14b4e22d9240fae968d90898ea4f654426f
Aman Raj <rajaman.gaya@gmail.com>
2016-02-08 17:53:07 +0530

    Added components home and app

---
+++ src/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <ul class="nav navbar-nav">
+          <li><a v-link="'home'">Home</a></li>
+          <li><a v-link="'login'">Login</a></li>
+          <li><a v-link="'signup'">Sign Up</a></li>
+          <li><a v-link="'secretquote'">Secret Quote</a></li>
+          <li><a v-link="'login'">Logout</a></li>
+        </ul>
+      </div>    
+    </nav>
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </template>
